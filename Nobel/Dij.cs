849e10d14d2ecb5f632f03a3e4a86d49a7679fc9
varashus <rigodavid100@gmail.com>
2023-10-10 08:37:07 +0200

    Add project files.

---
+++ Nobel/Dij.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobel
+{
+    class Dij
+    {
+        
+
+        public int ev { get; set; }
+        public string nev { get; set; }
+        public string szulhal { get; set; }
+        public (int Szuletes,int? Halal) SzH { get; set; }
+        public string orszkod { get; set; }
+
+
+
+        public Dij(string sor)
+        {
+            var r = sor.Split(';');
+            this.ev =int.Parse(r[0]);
+            this.nev = r[1];
+            this.szulhal = r[2];
+            var szh = r[2].Split("-");
+            int? H = szh[1].Length == 0 ? null : int.Parse(szh[1]);
+            SzH = (int.Parse(szh[0]), H);
+            this.orszkod = r[3];
+        }
+
+    }
+}
---
+++ Nobel/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Nobel
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                var emberek = new List<Dij>();
+                using var sr = new StreamReader(
+                    path: @"..\..\..\scr\orvosi_nobeldijak.txt",
+                    encoding: System.Text.Encoding.UTF8
+                );
+                _ = sr.ReadLine();
+                while (!sr.EndOfStream) emberek.Add(new(sr.ReadLine()));
+
+
+            Console.WriteLine($"3.feladat: {emberek.Count} fő");
+            int max = emberek.Max(n => n.ev);
+            Console.WriteLine($"4.feladat: {max} ");
+
+
+
+
+            Console.WriteLine("add mega az ember nemzetiseget: ");
+            string ok = Console.ReadLine();
+            var f5 = emberek.Where(d => d.orszkod == ok).ToList();
+            if (f5.Count == 0)
+            {
+                Console.WriteLine("nem volt");
+            }
+            else if(f5.Count == 1)
+            {
+                Console.WriteLine($"{f5[0].nev}");
+                Console.WriteLine($"{f5[0].ev}");
+                Console.WriteLine($"{f5[0].SzH}");
+                
+            }
+            
+            
+
+
+            Console.WriteLine($"6.feladat:");
+            
+
+
+
+            Console.ReadLine();
+            }
+            
+    }
+}
